8539b8102c9be5609ecc0884c2b8e51cfee4cf9a
ipadbook4u <144982110+ipadbook4u@users.noreply.github.com>
2025-07-12 17:44:49 +0700

    Add Express at predict.js

--- predict.js
+++ predict.js
@@ -1,3 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 10000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 const { Worker } = require("worker_threads");
 
 var myBitcoin = new Set([
